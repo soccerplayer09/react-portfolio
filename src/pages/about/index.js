80268c7cc5ddb32639e26e61f1b7b4f7cf6799bf
Donggoo Kim <donggookim@Donggoos-MacBook-Air.local>
2025-01-30 21:27:51 +0900

    refactor: remove unused sections

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -79,7 +79,7 @@ export const About = () => {
             })}
           </Col>
         </Row>
-        <Row className="sec_sp">
+        {/* <Row className="sec_sp">
           <Col lang="5">
             <h3 className="color_sec py-4">services</h3>
           </Col>
@@ -93,7 +93,7 @@ export const About = () => {
               );
             })}
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </HelmetProvider>
   );
